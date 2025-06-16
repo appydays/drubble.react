4c98ed2e14c1df0f4deac057ecb83918a53b6f83
dwpember <80764129+fifteendevelopers@users.noreply.github.com>
2025-06-16 14:36:26 +0100

    Add future plans to welcome page

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -7,6 +7,7 @@ const WelcomePage = ({ playerId, onPlayAsGuest, onLoginClick, onLogoutClick }) =
     const [playerName, setPlayerName] = useState(null);
     const [showHowToPlay, setShowHowToPlay] = useState(false); // State for expandable section
     const [showWhyRegister, setShowWhyRegister] = useState(false); // State for expandable section
+    const [showFuturePlans, setShowFuturePlans] = useState(false); // State for expandable section
 
     const baseUrl = process.env.REACT_APP_API_URL;
     const apiUrl = baseUrl + '/api';
@@ -157,6 +158,22 @@ const WelcomePage = ({ playerId, onPlayAsGuest, onLoginClick, onLogoutClick }) =
                             <div><b>Play as a guest to jump right into the fun</b>, or <b>register for free to unlock more!</b> Registered players can participate in daily leaderboards, track their game history, and view their detailed personal statistics.</div>
                             <br/>
                             <div>After you complete your daily challenge, be sure to share your score with friends and see who reigns supreme! Whether you're looking for a quick brain teaser, a fun way to pass the time, or a friendly competition with your friends, Drubble offers endless hours of engaging gameplay. Sharpen your mind, expand your vocabulary, and challenge yourself to become the ultimate wordsmith!</div>
+                        </div>
+                    )}
+                    <h2 className="section-title" onClick={() => setShowFuturePlans(!showFuturePlans)}>
+                        What plans are there for future development? <span className="toggle-icon">{showFuturePlans ? '▲' : '▼'}</span>
+                    </h2>
+                    {showFuturePlans && (
+                        <p className="section-content expanded">
+                            <p>We have a couple of ideas in the pipeline for future development and to enhance the experience:</p>
+                            <ol>
+                                <li>Add access to more stats, e.g. on results of previous days and months ,</li>
+                                <li>Implement a "learner" mode - enable players to hover over text for translations and meaning of words.</li>
+                                <li>Enhance statistics to enable players to filter leader boards by language level, so instead of displaying just the top 10 global leaders, the top 10 learners , or Intermediate or Advanced levels can be shown.</li>
+                                <li>We plan to add links to learning resources</li>
+                            </ol>
+                            <p>If you do have any features you would like to see in the game, we would love to hear from you, please either submit the feedback form or email us at support@scramair.cymru</p>
+                            <p>Or if you would like to hear of news and updates to Scramair , please do make sure you check your preferences and enable us to inlclude you on the regular newletters. You can opt in or opt out at any time. </p>
                         </div>
                     )}
                 </div>
