9eb78a9fec35318bad5243b4052573a6435c9e8b
dwpember <80764129+fifteendevelopers@users.noreply.github.com>
2025-06-16 20:53:31 +0100

    Remove future plans section

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -160,22 +160,22 @@ const WelcomePage = ({ playerId, onPlayAsGuest, onLoginClick, onLogoutClick }) =
                             <div>After you complete your daily challenge, be sure to share your score with friends and see who reigns supreme! Whether you're looking for a quick brain teaser, a fun way to pass the time, or a friendly competition with your friends, Drubble offers endless hours of engaging gameplay. Sharpen your mind, expand your vocabulary, and challenge yourself to become the ultimate wordsmith!</div>
                         </div>
                     )}
-                    <h2 className="section-title" onClick={() => setShowFuturePlans(!showFuturePlans)}>
-                        What plans are there for future development? <span className="toggle-icon">{showFuturePlans ? '▲' : '▼'}</span>
-                    </h2>
-                    {showFuturePlans && (
-                        <p className="section-content expanded">
-                            <p>We have a couple of ideas in the pipeline for future development and to enhance the experience:</p>
-                            <ol>
-                                <li>Add access to more stats, e.g. on results of previous days and months ,</li>
-                                <li>Implement a "learner" mode - enable players to hover over text for translations and meaning of words.</li>
-                                <li>Enhance statistics to enable players to filter leader boards by language level, so instead of displaying just the top 10 global leaders, the top 10 learners , or Intermediate or Advanced levels can be shown.</li>
-                                <li>We plan to add links to learning resources</li>
-                            </ol>
-                            <p>If you do have any features you would like to see in the game, we would love to hear from you, please either submit the feedback form or email us at support@scramair.cymru</p>
-                            <p>Or if you would like to hear of news and updates to Scramair , please do make sure you check your preferences and enable us to inlclude you on the regular newletters. You can opt in or opt out at any time. </p>
-                        </p>
-                    )}
+                    {/*<h2 className="section-title" onClick={() => setShowFuturePlans(!showFuturePlans)}>*/}
+                    {/*    What plans are there for future development? <span className="toggle-icon">{showFuturePlans ? '▲' : '▼'}</span>*/}
+                    {/*</h2>*/}
+                    {/*{showFuturePlans && (*/}
+                    {/*    <p className="section-content expanded">*/}
+                    {/*        <p>We have a couple of ideas in the pipeline for future development and to enhance the experience:</p>*/}
+                    {/*        <ol>*/}
+                    {/*            <li>Add access to more stats, e.g. on results of previous days and months ,</li>*/}
+                    {/*            <li>Implement a "learner" mode - enable players to hover over text for translations and meaning of words.</li>*/}
+                    {/*            <li>Enhance statistics to enable players to filter leader boards by language level, so instead of displaying just the top 10 global leaders, the top 10 learners , or Intermediate or Advanced levels can be shown.</li>*/}
+                    {/*            <li>We plan to add links to learning resources</li>*/}
+                    {/*        </ol>*/}
+                    {/*        <p>If you do have any features you would like to see in the game, we would love to hear from you, please either submit the feedback form or email us at support@scramair.cymru</p>*/}
+                    {/*        <p>Or if you would like to hear of news and updates to Scramair , please do make sure you check your preferences and enable us to inlclude you on the regular newletters. You can opt in or opt out at any time. </p>*/}
+                    {/*    </p>*/}
+                    {/*)}*/}
                 </div>
 
                 {/* Footer or additional links */}
