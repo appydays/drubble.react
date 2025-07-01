663bd826ac2675c7e625e01aaf17d7539bd9fcba
dwpember <80764129+fifteendevelopers@users.noreply.github.com>
2025-07-01 20:23:34 +0100

    Update leaderboard stats

--- src/components/Panagram.js
+++ src/components/Panagram.js
@@ -579,7 +579,7 @@ const Panagram = ({
                     onClose={handleAccountModalClose}
                     onSignupSuccess={handleSignupSuccess}
                     onLoginSuccess={handleLoginSuccess}
-                    player={localPlayer} {/* Pass localPlayer to modal */}
+                    player={localPlayer}
                     onPlayerUpdate={handlePlayerUpdate}
                 />
                 <SettingsModal
