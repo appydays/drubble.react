c8d7c632b8f7e2daf8fd98f0151683a9c2517aac
dwpember <80764129+fifteendevelopers@users.noreply.github.com>
2025-08-25 10:48:08 +0100

    Release 1.3.1 - added words used to return leaderboard data

--- src/components/DailyLeaderboardTable.js
+++ src/components/DailyLeaderboardTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { useTranslation } from 'react-i18next'; // Assuming you're using react-i18next for 't'
 import Modal from './Modal';
 
